c2b409d35df744de8581cc02ed41a3f3071de715
Dodge W. Coates <dodge.w.coates@gmail.com>
2023-10-15 14:07:40 -0400

    Add vim-matchup.

--- init.lua
+++ init.lua
@@ -76,6 +76,11 @@ require('lazy').setup({
 
   {'tpope/vim-surround'},
 
+  {
+    'andymass/vim-matchup',
+    event = 'VimEnter',  -- Load vim-matchup on VimEnter event
+  },
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
